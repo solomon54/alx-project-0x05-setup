1a254159e7d47f7f5ec168cbb3efbe80d3e5d3ba
solomon54 <solomontsehay54@gmail.com>
2025-12-04 21:31:42 +0300

    Applying the react state

---
+++ alx-project-0x08/pages/index.tsx
@@ -0,0 +1,34 @@
+import ImageCard from "../components/common/ImageCard";
+import { ImageProps } from "../interfaces";
+import React , { useState } from "react";
+const Home: React.FC = () => {
+  const [prompt, setPrompt] = useState<string>('');
+  const [imageUrl, setImageUrl] = useState<string>('');
+  const [generatedImages, setGeneratedImages] = useState<ImageProps[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const handleGenerateImage = async () => {
+    console.log("Generating Images");
+ 
+};
+
+return(
+  <div className="flex flex-col items-center min-h-screen bg-gray-100 p-4">
+    <div className="flex flex-col items-center">
+      <h1 className="text-4xl font-bold mb-2 text-indigo-900"> Image Generation App</h1>
+      <p className="text-lg text-gray-700 mb-4"> Generating stuning images based on your prompts!</p>
+      <div>
+        <input type="text" value={prompt} onChange={(e) => setPrompt(e.target.value)} placeholder="Enter your prompt here..." className="w-full p-3 border-gray-600 rounded-lg mb-4 text-gray-500"/>
+        <button onClick={handleGenerateImage} className="w-full p-3 bg-linear-to-r from bg-indigo-600 to-red-600 text-white rounded-lg hover:bg-blue-700  font-semibold hover:scale-103 active:scale-100 transition duration-300"> 
+          {/* {isLoading ? 'Loading...': Generate Image} */}
+          Generate Image</button>
+      </div>
+
+      {imageUrl && <ImageCard action={() => setImageUrl(imageUrl)} imageUrl={imageUrl} prompt={prompt}/>}
+    </div>
+
+  </div>
+);
+}
+
+export default Home;
