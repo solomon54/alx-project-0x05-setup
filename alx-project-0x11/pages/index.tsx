cd0b1c6a77c64bd8bbbbd27a8f84e5aea94468c7
solomon54 <solomontsehay54@gmail.com>
2025-12-05 12:02:30 +0300

    Track generated images and diplay them

--- alx-project-0x11/pages/index.tsx
+++ alx-project-0x11/pages/index.tsx
@@ -36,12 +36,12 @@ const Home: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col items-center min-h-screen bg-gray-100 p-4">
+    <div className="flex flex-col items-center min-h-screen bg-linear-to-r from-green-600 to-indigo-700 p-4">
       <div className="flex flex-col items-center">
-      <h1 className="text-4xl font-bold mb-2 text-indigo-900"> Image Generation App</h1>
-      <p className="text-lg text-gray-700 mb-4"> Generating stuning images based on your prompts!</p>
+      <h1 className="text-4xl font-bold mb-2 text-gray-300"> Image Generation App</h1>
+      <p className="text-lg text-gray-200 mb-4"> Generating stuning images based on your prompts!</p>
       <div>
-        <input type="text" value={prompt} onChange={(e) => setPrompt(e.target.value)} placeholder="Enter your prompt here..." className="w-full p-3 border-gray-600 rounded-lg mb-4 text-gray-500"/>
+        <input type="text" value={prompt} onChange={(e) => setPrompt(e.target.value)} placeholder="Enter your prompt here..." className="w-full p-3 border-red-400 rounded-lg mb-4 text-gray-300"/>
         <button onClick={handleGenerateImage} className="w-full p-3 bg-linear-to-r from bg-indigo-600 to-red-600 text-white rounded-lg hover:bg-blue-700  font-semibold hover:scale-103 active:scale-100 transition duration-300"> 
           {isLoading ? 'Loading...': 'Generate Image'}
        </button>
